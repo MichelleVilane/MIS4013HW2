2bd2841f27a5af058da74b9da258791d0df1ce34
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-09-17 23:56:52 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,1 +1,14 @@
-
+<?php
+$pageTitle= "Post result";
+include "view-header.php";
+?>
+ <h1>Post result</h1>
+<?php
+if (isset($_POST['my-name'])) {
+?>
+  <p>Nothing posted to the page.</p>
+<?php
+} else {
+}
+include "view-footer.php";
+?>
